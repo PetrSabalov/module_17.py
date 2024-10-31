8ef6cf7fef58772427a6fdb3e96b2bfcde08cbdc
PetrSabalov <petrsabalov@gmail.com>
2024-10-31 22:55:50 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,21 @@
+from sys import prefix
+
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/task", tags=["task"])
+
+@router.get("/")
+async def all_tasks():
+    pass
+
+@router.get("/task_id")
+async def task_by_id():
+    pass
+
+@router.post("/create")
+async def create_task():
+    pass
+
+@router.put("/update")
+async def delete_task():
+    pass
